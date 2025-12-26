4f6a3391a0d1b00df902815635c403fc3285b222
rdgztorres19 <rdgztorres19@gmail.com>
2025-12-26 14:36:52 -0500

    Delete ghost-blog/versions/6.10.3 directory

--- ghost-blog/versions/6.10.3/core/frontend/web/middleware/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    cors: require('./cors'),
-    errorHandler: require('./error-handler'),
-    frontendCaching: require('./frontend-caching'),
-    handleImageSizes: require('./handle-image-sizes'),
-    redirectGhostToAdmin: require('./redirect-ghost-to-admin'),
-    servePublicFile: require('./serve-public-file'),
-    staticTheme: require('./static-theme')
-};
